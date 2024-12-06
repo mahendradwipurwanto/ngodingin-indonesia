fa04bf20f8d1e0920c0d7d6ca337b03c6d519e0c
Mahendra Dwi Purwanto <mahendradwipurwanto@gmail.com>
2024-12-06 23:13:01 +0700

    feat(home): coming soon page

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,22 +1,9 @@
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-
 export default function HomePage() {
-  const t = useTranslations('homepage');
   return (
-    <div className='container mx-auto flex min-h-screen flex-col items-center justify-center gap-4 text-center'>
-      <Image
-        src='/assets/images/icon.png'
-        width={80}
-        height={80}
-        alt='logo'
-        className='rounded-md'
-      />
-
-      <div>
-        <h1 className='text-primary-70 h4-700'>Ngodingin Indonesia</h1>
-        <p className='text-gray-100'>{t('coming_soon')}</p>
-      </div>
+    <div className='flex h-screen w-screen items-center justify-center bg-black dark:bg-white'>
+      <h1 className='relative w-full animate-anim text-center text-5xl uppercase leading-[0.70em] tracking-[10px] outline-none box-reflect sm:tracking-[17px] md:text-8xl xl:text-9xl'>
+        Coming Soon
+      </h1>
     </div>
   );
 }
--- helper/index.tsx
+++ helper/index.tsx
@@ -1,2 +1,1 @@
 export const NODE_TARGET = process.env.NEXT_PUBLIC_NODE_TARGET;
-export const HOSTNAME = process.env.NEXT_PUBLIC_HOSTNAME;
